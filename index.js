7902ffac729680209253a85834fd4cd43c2a4697
AlenaKhanis <alen4k245@gmail.com>
2024-08-16 10:11:17 +0300

    model load

---
+++ index.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const modelViewer = document.getElementById('myModel');
+    const button = document.getElementById('button');
+
+    button.addEventListener('click', function () {
+        window.location.href = 'https://www.google.com/';
+    });
+
+});
