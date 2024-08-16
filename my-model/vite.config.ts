05d819c3ef4c57273c97568ac88d9e43f42fdc4d
AlenaKhanis <alen4k245@gmail.com>
2024-08-16 15:18:59 +0300

    pages

--- my-model/vite.config.ts
+++ my-model/vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/AlonaKhanis/3D-Model',
+  base: '/3D-Model/', // Correctly specify the subpath for GitHub Pages
   plugins: [react()],
-})
+});
