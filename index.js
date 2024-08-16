823621a4a23ba714a6b1170a40842d7493bc3468
AlenaKhanis <alen4k245@gmail.com>
2024-08-16 10:52:22 +0300

    + react

--- index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const modelViewer = document.getElementById('myModel');
-    const button = document.getElementById('button');
-
-    button.addEventListener('click', function () {
-        window.location.href = 'https://www.google.com/';
-    });
-
-});
